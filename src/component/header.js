f3579f843287b289a6dc39d9cb8a0b4ae4d7e2ae
hemantsharma2004 <125672327+hemantsharma2004@users.noreply.github.com>
2024-01-26 14:42:01 +0530

    Update header.js
    Header

--- src/component/header.js
+++ src/component/header.js
@@ -1,44 +1,48 @@
 import { useState } from "react";
+import useOnlineStatus from "../../utils/useOnlineStatus";
+import { useSelector } from "react-redux";
+import Cart from "./cart";
 import { Link } from "react-router-dom";
 
-const Header=() =>{
+const Header=()=>{
+ const [Button , setButton]= useState("login");
+ 
+  const Online=useOnlineStatus();
 
-    const[ButtonReact, setButtonReact] = useState("login");
+   const cartItems=useSelector((store)=> store.cart.items);
 
+     return (
+           <div className="parent mb-10 flex justify-between items-center px-2  pt-0  Border border-red-700 shadow-lg">
+            
+             <div>
+                 <img className="w-[120px]" src="https://i.pinimg.com/originals/b3/8a/a1/b38aa1b21050b0e769a97eb751d12829.png"></img>
+             </div>
 
-     return(
-         <div className="header">
-              <div className="logo">
-                  <img src="https://i.pinimg.com/736x/b3/8a/a1/b38aa1b21050b0e769a97eb751d12829.jpg"></img>
-              </div>
+             <div>
+                  <ul className="flex w-[500px] text-lg">
+                    <li className="mr-5">{Online ? "🟢" : "🔴" }</li>
+                    <li className="mr-4">
+                      <Link to="/" >home</Link>
+                      </li>
 
-              <div className="nav-links">
-                  <ul className="flex">
-                     <li>
-                         < Link to="/">home</Link>
-                        </li>
+                    <li className="mr-4">
+                      <Link to="contact"> contact</Link>
+                      </li>
 
-                        <li>
-                         < Link to="/About">About us</Link>
-                        </li>
+                    <li className="mr-4">
+                     <Link to="about"> about </Link> 
+                      </li>
 
-                        <li>
-                         < Link to="/Contact">Contact us</Link>
-                        </li>
-
-                       
-                        
-
-                     
-
-                     <button onClick={() =>{
-                         const button="logout";
-                         setButtonReact(button);
-                     }}>{ButtonReact}</button>
-
+                    <li className="mr-7">
+                     <Link to="cart"> 🛒 ({cartItems.length} Items)</Link> 
+                      </li>
+                    <button className="bg-orange-500 text-lg p-1 rounded-lg text-white" onClick={()=>{
+                       const buttons="logout";
+                       setButton(buttons);
+                    }}>{Button}</button>
                   </ul>
-              </div>
-         </div>
+             </div>
+           </div>
      )
 }
 
