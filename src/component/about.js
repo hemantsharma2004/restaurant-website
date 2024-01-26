ec73f9a8499a94d48a9a3dbca40a3fde77665a1e
hemantsharma2004 <125672327+hemantsharma2004@users.noreply.github.com>
2024-01-26 14:37:20 +0530

    Update about.js
    About Us Updated

--- src/component/about.js
+++ src/component/about.js
@@ -1,19 +1,9 @@
-import React from "react";
-import Hello from "./function";
-import Home from "./class";
 
-
-  class About extends React.Component{
-     render()
-     {
-        return(
-            <div>
-             <Hello name={"first"} location={"dehradun"}/>
-   
-             <Home name={"second"} location={"dehradun 🚩"}/>
-            </div>
-        )
-     }
-  }
-   export default About;
-
+const About=()=>{
+     return (
+         <div>
+             <h1>hello about us page</h1>
+         </div>
+     )
+}
+export default About;
